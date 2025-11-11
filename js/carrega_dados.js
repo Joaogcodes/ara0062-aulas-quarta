5007dd3acefb4c1974401104d54d1a722743766f
Joaogcodes <joaoguilhermeoliveira1224@gmail.com>
2025-11-11 17:15:39 -0300

    Update carrega_dados.js

--- js/carrega_dados.js
+++ js/carrega_dados.js
@@ -1,26 +1,27 @@
-const tabela = document.getElementById("tabela-livros");
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("js/dados.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Erro ao carregar dados.json");
+      }
+      return response.json();
+    })
+    .then(data => {
+      const tabela = document.getElementById("tabela-livros");
 
-fetch("js/dados.json")
+      data.livros.forEach(livro => {
+        const linha = document.createElement("tr");
 
-  .then(response => response.json())
-  .then(livros => {
-    livros.forEach(livro => {
-      const linha = document.createElement("tr");
+        linha.innerHTML = `
+          <td><img src="${livro.capa}" alt="Capa do livro" class="capa-img" width="80"></td>
+          <td>${livro.titulo}</td>
+          <td>${livro.autor}</td>
+          <td>${livro.genero}</td>
+          <td>${livro.ano}</td>
+        `;
 
-      const celulaCapa = document.createElement("td");
-      const img = document.createElement("img");
-      img.src = "img/" + livro.capa; // <-- CAMINHO AJUSTADO
-      img.width = 80;
-      celulaCapa.appendChild(img);
-
-      linha.innerHTML += `
-        <td>${livro.titulo}</td>
-        <td>${livro.autor}</td>
-        <td>${livro.genero}</td>
-        <td>${livro.ano}</td>
-      `;
-
-      linha.prepend(celulaCapa);
-      tabela.appendChild(linha);
-    });
-  });
+        tabela.appendChild(linha);
+      });
+    })
+    .catch(error => console.error(error));
+});
