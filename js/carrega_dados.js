f27fe88015ccaed5f8401486754fc2f492c060c1
Joaogcodes <joaoguilhermeoliveira1224@gmail.com>
2025-11-12 00:08:49 -0300

    Fix fetch path and add width to book cover images

--- js/carrega_dados.js
+++ js/carrega_dados.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("../dados.json") // se catalogo.html estiver dentro de uma pasta, ajuste o caminho
+  fetch("dados.json") // <- CORRETO, sem "../"
     .then(response => response.json())
     .then(data => {
       const tabela = document.getElementById("tabela-livros");
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
       data.livros.forEach(livro => {
         const linha = document.createElement("tr");
         linha.innerHTML = `
-          <td><img src="${livro.capa}" alt="${livro.titulo}" class="capa-img"></td>
+          <td><img src="${livro.capa}" alt="${livro.titulo}" class="capa-img" width="80"></td>
           <td>${livro.titulo}</td>
           <td>${livro.autor}</td>
           <td>${livro.genero}</td>
