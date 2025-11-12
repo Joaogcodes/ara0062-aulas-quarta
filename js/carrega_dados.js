bc11ef352b3b3728ce78ef9f4f4cc63a0becc07f
JoaoGcodes <joaoguilhermeoliveira1224@gmail.com>
2025-11-11 23:49:43 -0300

    Primeiro commit

--- js/carrega_dados.js
+++ js/carrega_dados.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+<<<<<<< HEAD
   fetch("dados.json") // <- CORRETO, sem "../"
+=======
+  fetch("../dados.json") // se catalogo.html estiver dentro de uma pasta, ajuste o caminho
+>>>>>>> 17723ac (Primeiro commit)
     .then(response => response.json())
     .then(data => {
       const tabela = document.getElementById("tabela-livros");
@@ -7,7 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
       data.livros.forEach(livro => {
         const linha = document.createElement("tr");
         linha.innerHTML = `
+<<<<<<< HEAD
           <td><img src="${livro.capa}" alt="${livro.titulo}" class="capa-img" width="80"></td>
+=======
+          <td><img src="${livro.capa}" alt="${livro.titulo}" class="capa-img"></td>
+>>>>>>> 17723ac (Primeiro commit)
           <td>${livro.titulo}</td>
           <td>${livro.autor}</td>
           <td>${livro.genero}</td>
